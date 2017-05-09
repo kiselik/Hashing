83adf62986e806c16d6133a86792399022957d83
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-05-09 16:28:01 +0300

    added implementation class Record

--- include/Record.h
+++ include/Record.h
@@ -12,4 +12,24 @@ private:
     K key_;
     V value_;
 };
-
+template<typename K, typename V>
+Record<K, V>::Record(K key, V value) {
+    key_ = key;
+    value_ = value;
+}
+template<typename K, typename V>
+void Record<K, V>::SetKey(K key) {
+    key_ = key;
+}
+template<typename K, typename V>
+K Record<K, V>::GetKey() {
+    return key_;
+}
+template<typename K, typename V>
+void Record<K, V>::SetValue(Vvalue) {
+    value_ = value;
+}
+template<typename K, typename V>
+K Record<K, V>::GetValue() {
+    return value_;
+}
